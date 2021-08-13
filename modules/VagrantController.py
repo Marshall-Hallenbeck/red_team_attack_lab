24efe754aadcbe74af2065796c7418ade0ef4a71
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2021-08-13 15:08:53 -0400

    Semi-working deployment. DC and the first win-client work, the second fails to rename?

--- modules/VagrantController.py
+++ modules/VagrantController.py
@@ -176,9 +176,11 @@ class VagrantController():
                     self.log.info(dump_info)
                     out = open(os.path.join(os.path.dirname(__file__), "../attack_data/" + dump_name + "/" + dump_out),
                                'wb')
-                    splunk_sdk.export_search(self.config['splunk_server_private_ip'],
-                                             s=dump_search,
-                                             password=self.config['splunk_admin_password'],
-                                             out=out)
+                    splunk_sdk.export_search(
+                        self.config['splunk_server_private_ip'],
+                        s=dump_search,
+                        password=self.config['splunk_admin_password'],
+                        out=out
+                    )
                     out.close()
                     self.log.info("%s [Completed]" % dump_info)
