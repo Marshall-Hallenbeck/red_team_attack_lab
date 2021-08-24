acfded0e3a1e3d55a44725fbfd3c8bb695d420ea
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2021-08-23 22:10:14 -0400

    update deploy script

--- deploy_attack_lab.sh
+++ deploy_attack_lab.sh
@@ -5,8 +5,9 @@ sudo apt-get update
 sudo apt-get install -y python3-dev linux-headers-generic python-dev unzip python-pip vagrant virtualbox virtualbox-dkms python-virtualenv git
 sudo gem install winrm-elevated
 sudo gem install winrm
-git clone https://github.com/splunk/attack_range_local && cd attack_range_local
+git clone https://github.com/Marshall-Hallenbeck/red_team_attack_lab && cd red_team_attack_lab
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
 ansible-galaxy collection install community.windows chocolatey.chocolatey
+python red_team_attack_lab.py -a build
