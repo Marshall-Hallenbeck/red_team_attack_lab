d5c3f54515cc559a2087b3cce0182ef262424e2b
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2021-08-24 23:58:36 -0400

    do some cleaning/optimizing on rtal.py and VagrantController

--- modules/VagrantController.py
+++ modules/VagrantController.py
@@ -37,7 +37,7 @@ class VagrantController:
         self.vagrantfile += '\nend'
         with open('vagrant/Vagrantfile', 'w') as file:
             file.write(self.vagrantfile)
-        self.log.info("Wrote Vagrantfile\n")
+        self.log.info("Wrote Vagrantfile")
 
     def read_vagrant_file(self, path):
         j2_env = Environment(loader=FileSystemLoader('vagrant'), trim_blocks=True)
@@ -46,7 +46,7 @@ class VagrantController:
         return vagrant_file
 
     def build(self):
-        self.log.info("[action] > build\n")
+        self.log.info("Running build...")
         v1 = vagrant.Vagrant('vagrant/', quiet_stdout=False, quiet_stderr=False)
         try:
             v1.up(provision=True, provider="virtualbox")
@@ -58,49 +58,32 @@ class VagrantController:
         self.list_machines()
 
     def destroy(self):
-        self.log.info("[action] > destroy\n")
+        self.log.info("Running destroy...")
         v1 = vagrant.Vagrant('vagrant/', quiet_stdout=False)
         v1.destroy()
         self.log.info("attack_lab has been destroy using vagrant successfully")
 
     def stop(self):
-        print("[action] > stop\n")
+        self.log.info("Running stop...")
         v1 = vagrant.Vagrant('vagrant/', quiet_stdout=False)
         v1.halt()
 
     def resume(self):
-        print("[action] > resume\n")
+        self.log.info("Running resume...")
         v1 = vagrant.Vagrant('vagrant/', quiet_stdout=False)
         v1.up()
 
-    def get_ip_address_from_machine(self, box):
-        pattern = 'config.vm.define "' + box + '"[\s\S]*?:private_network, ip: "([^"]+)'
-        match = re.search(pattern, self.vagrantfile)
-        return match.group(1)
-
-    def check_targets_running_vagrant(self, target, log):
-        v1 = vagrant.Vagrant('vagrant/', quiet_stdout=False)
-        status = v1.status()
-
-        found_box = False
-        for stat in status:
-            if stat.name == target:
-                found_box = True
-                if not (stat.state == 'running'):
-                    log.error(target + ' not running.')
-                    sys.exit(1)
-                break
-        if not found_box:
-            log.error(target + ' not found as vagrant box.')
-            sys.exit(1)
-
     def list_machines(self):
-        print()
-        print('Vagrant Status\n')
+        self.log.info('Getting Vagrant status...')
         v1 = vagrant.Vagrant('vagrant/', quiet_stdout=False)
         response = v1.status()
         status = []
         for stat in response:
             status.append([stat.name, stat.state, self.get_ip_address_from_machine(stat.name)])
 
-        print(tabulate(status, headers=['Name', 'Status', 'IP Address']))
+        self.log.info("Vagrant status:\n" + tabulate(status, headers=['Name', 'Status', 'IP Address']))
+
+    def get_ip_address_from_machine(self, box):
+        pattern = 'config.vm.define "' + box + '"[\s\S]*?:private_network, ip: "([^"]+)'
+        match = re.search(pattern, self.vagrantfile)
+        return match.group(1)
--- red_team_attack_lab.py
+++ red_team_attack_lab.py
@@ -13,21 +13,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "-a", "--action",
         required=False,
-        choices=['build', 'create_config', 'destroy', 'stop', 'resume'],
-        help="action to take in the lab, defaults to \"build\", build/destroy/stop/resume allowed"
+        choices=['build', 'create_config', 'destroy', 'stop', 'resume', 'status'],
+        default='create_config',
+        help="action to take in the lab: build/create_config/destroy/stop/resume/status allowed \ndefault: create_config"
     )
     parser.add_argument(
         "-c", "--config",
         required=False,
         default="attack_lab.conf",
         help="path to the configuration file of the attack range"
-    )
-    parser.add_argument(
-        "-lm", "--list_machines",
-        required=False,
-        default=False,
-        action="store_true",
-        help="prints out all available machines"
     )
     parser.add_argument(
         "-v", "--version",
@@ -41,7 +35,6 @@ if __name__ == "__main__":
     ARG_VERSION = args.version
     action = args.action
     config = args.config
-    list_machines = args.list_machines
 
     print("""
     Starting Red Team Attack Lab...
@@ -49,35 +42,24 @@ if __name__ == "__main__":
     Original: Splunk Attack Range
     """)
 
-    # parse config
     attack_lab_config = Path(config)
     if attack_lab_config.is_file():
-        print("attack_lab is using config at path {0}".format(attack_lab_config))
+        print("attack_lab is using config at path {0}\n".format(attack_lab_config))
         config_path = str(attack_lab_config)
     else:
         print("ERROR: attack_lab failed to find a config file at {0}..exiting".format(attack_lab_config))
         sys.exit(1)
 
-    # Parse config
     parser = CustomConfigParser()
     config = parser.load_conf(config_path)
 
     log = logger.setup_logging(config['log_path'], config['log_level'])
-    log.info("INIT - attack_lab v" + str(VERSION))
 
     if ARG_VERSION:
         log.info("version: {0}".format(VERSION))
         sys.exit(0)
 
-    if not action and not list_machines:
-        log.error('ERROR: Use -a to perform an action or -lm to list available machines')
-        sys.exit(1)
-
     controller = VagrantController(config, log)
-
-    if list_machines:
-        controller.list_machines()
-        sys.exit(0)
 
     if action == 'create_config':
         pass
@@ -93,3 +75,6 @@ if __name__ == "__main__":
 
     if action == 'resume':
         controller.resume()
+
+    if action == 'status':
+        controller.list_machines()
