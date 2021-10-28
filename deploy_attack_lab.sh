d306c4c8e0df3ce27a8d65b594872ae93e4729d0
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2021-10-27 22:59:01 -0400

    requirements: update deploy script for proper requirements

--- deploy_attack_lab.sh
+++ deploy_attack_lab.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update 
-sudo apt-get install -y linux-headers-generic vagrant virtualbox virtualbox-dkms
+sudo apt update
+sudo apt install -y linux-headers-generic vagrant ansible
+sudo apt install -y virtualbox virtualbox-dkms
 sudo gem install winrm-elevated
 sudo gem install winrm
+pip install "pywinrm>=0.2.2"
 git clone https://github.com/Marshall-Hallenbeck/red_team_attack_lab
 cd red_team_attack_lab
 ansible-galaxy collection install community.windows chocolatey.chocolatey
